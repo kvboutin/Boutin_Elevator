805c3dc8df5b9254457de9344886871c4a1d088e
Kaelyn Boutin <kvboutin@mines.edu>
2024-03-13 18:00:48 -0600

    i/o implemented

--- src/Control.java
+++ src/Control.java
@@ -1,9 +1,42 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Control {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
-
+		System.out.print("Please enter the floor you are starting on: ");
+		int start = 0;
+		try {
+			start = input.nextInt();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.print("Please enter how many floors will this trip will have: ");
+		//add error catching
+		int size = 0;
+		size = input.nextInt();
+		
+		System.out.println("Please enter the numbers of the floors you will be visiting in order: ");
+		
+		int visits[] = new int[size + 1];
+		visits[0] = start;
+		int floor = 0;
+		for(int i = 0; i < size; i++) {
+			floor = input.nextInt();
+			visits[i+1] = floor;	
+		}
+		
+		Trip test = new Trip(start, visits);
+		Elevator elevator1 = new Elevator();
+		
+		System.out.println("The time the trip took is: " + test.take_trip(elevator1));
+		System.out.println("The floors vistied in order are: " + Arrays.toString(visits));
 	}
 
 }
--- src/Trip.java
+++ src/Trip.java
@@ -2,20 +2,24 @@ import java.util.ArrayList;
 
 public class Trip {
 	public int start_floor;
-	public ArrayList<Integer> stops = new ArrayList<Integer>();
+	public int[] stops;
 	
 	public Trip() {
 		super();
 		this.start_floor = 0;
 	}	
 	
-	public Trip(int start_floor, ArrayList<Integer> stops) {
+	public Trip(int start_floor, int[] stops) {
 		super();
 		this.start_floor = start_floor;
 		this.stops = stops;
 	}
 	
-	public int take_trip() {
+	public void setStops(int[] stops) {
+		this.stops = stops;
+	}
+
+	public int take_trip(Elevator elevator) {
 		return 0;
 	}
 	
