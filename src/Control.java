6196558cb54e3277dc94d9b5c2cd4fb0ed5921bb
Kaelyn Boutin <kvboutin@mines.edu>
2024-03-15 10:44:11 -0600

    refactored control and added input type checking

--- src/Control.java
+++ src/Control.java
@@ -1,41 +1,80 @@
+/**
+ * Control contains main, manages I/O and stores the elevator and trip objects.
+ * Also, contains a method to further test the code.
+ */
+
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Control {
+	public Scanner input = new Scanner(System.in); //variables needed for I/O
+	public boolean checkIn;						   //used to check for correct input type	
+	public int start;
+	public int size;
+	public int visits[];
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Please enter the floor you are starting on: ");
-		int start = 0;
-		try {
-			start = input.nextInt();
-		} catch (InputMismatchException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void getUserStart() {				   //Get a start floor from the user
+		System.out.print("Please enter the floor you are starting on: ");			
+
+		while(!checkIn) {
+			try {
+				start = input.nextInt();
+				checkIn = true;
+			} catch (InputMismatchException e) {
+				System.out.print("Enter an integer: ");
+				input.next();
+			}
 		}
-		
-		System.out.print("Please enter how many floors will this trip will have: ");
-		//add error catching
-		int size = 0;
-		size = input.nextInt();
-		
+		checkIn = false;
+	}
+
+	public void getUserVisits() {				  //Populate the list of floors that will be visited
+		System.out.print("Please enter how many floors will this trip will have: "); //Need the size for the array
+
+		while(!checkIn) {
+			try {
+				this.size = input.nextInt();
+				checkIn = true;
+			} catch (InputMismatchException e) {
+				System.out.print("Enter an integer: ");
+				input.next();
+			}
+		}
+
 		System.out.println("Please enter the numbers of the floors you will be visiting in order: ");
-		
-		int visits[] = new int[size + 1];
+
+		this.visits = new int[this.size + 1];			//Initialize and populate the visits array
 		visits[0] = start;
+		checkIn = false;
 		int floor = 0;
-		for(int i = 0; i < size; i++) {
-			floor = input.nextInt();
-			visits[i+1] = floor;	
+		for(int i = 1; i < this.size + 1; i++) {
+			while(!checkIn) {
+				try {
+					floor = input.nextInt();
+					checkIn = true;
+				} catch (Exception e) {
+					System.out.print("Enter an integer: ");
+					input.next();
+				}
+			}
+			visits[i] = floor;	
+			checkIn = false;
 		}
-		
-		Trip test = new Trip(start, visits);
+	}
+	
+	
+
+	public static void main(String[] args) {	//Store objects and call methods
+		Control control = new Control();
+
+		control.getUserStart();
+		control.getUserVisits();
+
+		Trip test = new Trip(control.start, control.visits);
 		Elevator elevator1 = new Elevator();
-		
-		System.out.println("The time the trip took is: " + test.take_trip(elevator1));
-		System.out.println("The floors vistied in order are: " + Arrays.toString(visits));
+
+		System.out.println("Time and floors visited: " + test.take_trip(elevator1) + ", " + Arrays.toString(control.visits));
 	}
 
 }
